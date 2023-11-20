f0cae01636e57b579bdb7b802cd533c04b12c20b
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-20 20:47:22 +0000

    Deploying to gh-pages from @ LMSerhii/goit-js-hw-09@994b24b82706ac01ee2a6d31135fd05ead991dca 🚀

---
+++ 01-color-switcher.16293434.js
@@ -0,0 +1,2 @@
+const t={start:document.querySelector("button[data-start]"),stop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;function o(){t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.start.addEventListener("click",(function(a){e=setInterval(o,1e3),t.start.disabled=!0,t.stop.disabled=!1})),t.stop.addEventListener("click",(function(){clearInterval(e),t.stop.disabled=!0,t.start.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.16293434.js.map
