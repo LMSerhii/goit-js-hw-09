29eaf886cbdd7af73fabf9b4f16588883d3892d6
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2023-11-23 05:57:51 +0000

    Deploying to gh-pages from @ LMSerhii/goit-js-hw-09@be95e3eadc3682fcabab31950f793d0dfd60e636 🚀

--- 03-promises.32e4c3ac.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".form"),l={delay:e.elements.delay.value,step:e.elements.step.value,amount:e.elements.amount.value};e.addEventListener("submit",(function(e){e.preventDefault(),console.log(l.delay),console.log(l.step),console.log(l.delay)}));
-//# sourceMappingURL=03-promises.32e4c3ac.js.map
--- 03-promises.8a26a154.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".form"),l={delay:e.elements.delay.value,step:e.elements.step.value,amount:e.elements.amount.value};e.addEventListener("submit",(function(e){e.preventDefault(),console.log(l.delay),console.log(l.step),console.log(l.delay)}))}();
-//# sourceMappingURL=03-promises.8a26a154.js.map
